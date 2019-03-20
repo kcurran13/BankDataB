e79c2a30b73492f2e207060b467a55d0306bfd62
Kate <curran.kate.t@gmail.com>
2019-03-20 12:25:50 +0100

    Building test GUI for withdraw/deposit

--- src/app/transaction/TransferControl.java
+++ src/app/transaction/TransferControl.java
@@ -3,11 +3,13 @@ package app.transaction;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 public class TransferControl {
-    @FXML TextField txfClearing, txfAccount;
-    @FXML ChoiceBox dropAcc;
+    @FXML TextField txfClearing, txfAccount, txfTestAmount;
+    @FXML ChoiceBox dropFromAcc, dropTestAcc;
+    @FXML Label lblTextBalance;
 
     @FXML
     private void initialize(){
