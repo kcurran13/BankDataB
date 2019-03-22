fc5c3fd5e6a8aa90408e4f2a362783516f9bba42
Kate <curran.kate.t@gmail.com>
2019-03-22 10:46:18 +0100

    Choicebox for accounts in transactions

--- src/app/account/AccountController.java
+++ src/app/account/AccountController.java
@@ -2,31 +2,57 @@ package app.account;
 
 
 import app.Entities.Transaction;
+import app.Entities.User;
 import app.db.DB;
+import app.login.LoginController;
 import app.transaction.TransactionController;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class AccountController {
 
-    @FXML
-    VBox transactionBox;
+    @FXML VBox transactionBox;
+    @FXML ChoiceBox dropAcc;
+    private User user;
+    ResultSet rs;
 
     @FXML
     private void initialize() {
         System.out.println("initialize app.account");
+        ObservableList<String> data = FXCollections.observableArrayList();
+
+        user = LoginController.getUser();
+
+        try {
+            rs = DB.getUserAccounts(user);
+            while(rs.next()) {
+                String item = rs.getString(1);
+                data.add(item);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        dropAcc.setItems(data);
+        dropAcc.getSelectionModel().select(0);
         loadMoreTransactions();
     }
 
     void loadMoreTransactions() {
-        List<Transaction> transactions = DB.getTransactions(11223344);
+        int accNo = Integer.valueOf(getChoiceBoxText(dropAcc));
+        List<Transaction> transactions = DB.getTransactions(accNo);
         displayTransaction(transactions);
     }
 
@@ -47,9 +73,14 @@ public class AccountController {
             }
     }
 
-
     @FXML
     void clickLoadTransactions(Event e) {
         loadMoreTransactions();
+    }
+
+    @FXML
+    private String getChoiceBoxText(ChoiceBox<String> box) {
+        String acc = box.getValue();
+        return acc;
     }
 }
